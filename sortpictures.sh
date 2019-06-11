3c9dba10bbd24f55e512018316d5d5e32506b747
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2019-06-11 12:50:10 +0200

    ADD checkFolder and movePicture functions

--- sortpictures.sh
+++ sortpictures.sh
@@ -4,10 +4,29 @@ function readDate {
    DATE=`identify -format %[EXIF:DateTime] $1`;
    YEAR=$(echo ${DATE::4});
    MONTH=$(echo ${DATE:5:2});
-   DAY=$(echo ${DATE:8:2});
-   echo $YEAR;
-   echo $MONTH;
-   echo $DAY;
 }
 
-readDate cc.jpg
+function checkFolder {
+    # check year folder
+    if [ ! -d $YEAR ];
+    then
+        echo 'Year Folder not found';
+        mkdir $YEAR;
+        echo 'Year Folder created';
+    fi
+    if [ ! -d $YEAR/$MONTH ];
+    then
+        echo 'Month Folder not found';
+        mkdir $YEAR/$MONTH;
+        echo 'Month Folder created';
+    fi
+}
+
+function movePicture {
+    mv $PICTURE $YEAR/$MONTH/$PICTURE;
+}
+
+PICTURE=$1;
+readDate $PICTURE;
+checkFolder;
+movePicture;
